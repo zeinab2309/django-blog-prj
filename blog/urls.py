13df5a5bfa531d507a82752a7979477ca4f01774
zeinab2309 <poormoosavie0@gmail.com>
2025-03-08 12:03:48 +0330

    change password

--- blog/urls.py
+++ blog/urls.py
@@ -1,13 +1,14 @@
+from django.contrib.messages import success
 from  django.urls import path
 from . import views
 from django.contrib.auth import views as auth_views
 app_name="blog"
 
 urlpatterns=[
-    path('',views.index , name="index"),#یو ار ال مخصوص صفحه محصولات است
-    #path('posts/', views.post_list, name="post_list"), #روی صفحه پست ها کلیک کنیم این یو ار ال را میاره
+    path('',views.index , name="index"),
+    #path('posts/', views.post_list, name="post_list"),
     path('posts/',views.PostListView.as_view() , name="post_list"),
-    path('posts/<int:pk>/', views.post_detail, name="post_detail"),#برای موقعی که روی یک پست بزنه ایدی جدا میده
+    path('posts/<int:pk>/', views.post_detail, name="post_detail"),
     path('posts/<int:post_id>/comment', views.post_comment, name="post_comment"),
     path('ticket', views.ticket, name="ticket"),
     path('search/',views.post_search, name="post_search"),
@@ -16,11 +17,12 @@ urlpatterns=[
     path('profile/create/<int:post_id>', views.edit_post, name="edit_post"),
     path('profile/delete_post/<int:post_id>', views.delete_post, name="delete_post"),
     path('profile/delete_image/<int:image_id>', views.delete_image, name="delete_image"),
-    # path('login', views.user_login,name="login")
+    # path('login', views.user_login,name="login"),
     path('login/', auth_views.LoginView.as_view(), name="login"),
     path('logout/', views.log_out, name="logout"),
-    path('password-change/',auth_views.PasswordChangeView.as_view(), name="password_change"),
+    path('password-change/',auth_views.PasswordChangeView.as_view(success_url='done'), name="password_change"),
     path('password-change/done/', auth_views.PasswordChangeDoneView.as_view(), name="password_change_done"),
+
 ]
 
 
